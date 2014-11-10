d57b9bd5b0d02efe8949f452e3152936884d72a7
Alexandre Moufarek <amoufarek@gmail.com>
2014-11-10 23:05:05 +0100

    Created blank HomePage controller in the Presentation Bundle

---
+++ src/DomaineFilliatreau/PresentationBundle/Controller/HomePageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DomaineFilliatreau\PresentationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomePageController extends Controller
+{
+    public function indexAction()
+    {
+        return;
+    }
+}
