2cc50cdfdd36498f2268aef39b55f88fe42e29ae
Alexandre Moufarek <amoufarek@gmail.com>
2014-11-11 15:22:01 +0100

    Updated HomePage controller to use get('templating') service

--- src/DomaineFilliatreau/PresentationBundle/Controller/HomePageController.php
+++ src/DomaineFilliatreau/PresentationBundle/Controller/HomePageController.php
@@ -3,11 +3,14 @@
 namespace DomaineFilliatreau\PresentationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomePageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DomaineFilliatreauPresentationBundle:HomePage:index.html.twig');
+        //return $this->render('DomaineFilliatreauPresentationBundle:HomePage:index.html.twig');
+        $content = $this->get('templating')->render('DomaineFilliatreauPresentationBundle:HomePage:index.html.twig');
+        return new Response($content);
     }
 }
